cf66544e65b15655bcb2b2cb928ffda4c5fe33d0
Daniel Moix <>
2021-09-07 20:44:53 +0000

    Made the background color a variable; Made the ball color change based on mouse position.

--- Sketch.java
+++ Sketch.java
@@ -2,16 +2,19 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
+  float bgcolor;
+
   public void settings() {
     size(400, 400);
   }
 
   public void setup() {
-    background(0);
+    bgcolor = 123;
   }
 
   public void draw() {
-    fill(255);
+    background(bgcolor);
+    fill(255 * mouseX / width, 0, 255 * mouseY / height);
     ellipse(mouseX, mouseY, 25, 25);
   }
 }
